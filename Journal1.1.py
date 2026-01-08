728087a80712cf4524de4e491b531f7f556c4f73
ttarelli <ttarelli@ucsc.edu>
2026-01-07 20:29:51 -0800

    Journal 1 Prompt 1

---
+++ Journal1.1.py
@@ -0,0 +1,4 @@
+print("Name:\tTate Tarelli")
+print("Prounouns:\tHe/Him")
+print("Favorite Movie:\tMy favorite movie is Harry Potter and the Goblet of Fire, which is also my favorite book.")
+print("Favorite Food:\tMy favorite food is pizza, specifically topped with pepperoni and jalepenos.")
