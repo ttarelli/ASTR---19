2799a3ecf4f025d4c65de643c63f4c103ac46012
ttarelli <ttarelli@ucsc.edu>
2026-01-12 18:09:38 -0800

    Journal 1 Prompt 2

---
+++ Journal1.2.py
@@ -0,0 +1,11 @@
+a = 5
+b = 4
+c = 6.72
+d = 8.35
+e = c + d
+f = a - b
+g = a * c
+print("a = 5, b = 4, c = 6.72 and d = 8.35")
+print(f"The sum of c and d is {e} and it is of type {type(e)}")
+print(f"The difference of a and b is {f} and it is of type {type(f)}")
+print(f"And the product of a and c is {g} and it is of type {type(g)}")
